452634d531d367517c24a19801f60c2674908015
Zac Smith <48374908+ZacDSmith@users.noreply.github.com>
2024-09-24 16:58:56 -0400

    Base text based rpg with one encounter

---
+++ Encounters.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedRpgProject
+{
+    class Encounters
+    {
+        static Random rand = new Random();
+        //Encounter Generic
+
+
+
+
+        //Ecounters
+        public static void firstEncounter()
+        {
+            Console.WriteLine("PlaceHolder First Encounter Text");
+            Console.ReadKey();
+            combat(false, "Human Rogue",1,4);
+
+        }
+        //Ecounter Tools
+        public static void combat(bool random, string name, int power, int health) 
+        {
+            string n = "";
+            int p = 0;
+            int h = 0;
+
+            if (random)
+            {
+
+            }
+            else
+            {
+                n = name;
+                p = power;
+                h = health;
+            }
+            while(h>0)
+            {
+                Console.Clear();
+                Console.WriteLine(n);
+                Console.WriteLine(p + "/" +h);
+                Console.WriteLine("===================");
+                Console.WriteLine("|(A)ttack (D)efend|");
+                Console.WriteLine("|(R)un    (H)eal  |");
+                Console.WriteLine("===================");
+                Console.WriteLine(" Potions: " + Program.currentPlayer.potions + " Health: "+Program.currentPlayer.health);
+                string? input = Console.ReadLine();
+                if (input.ToLower() == "a" || input.ToLower()=="attack") 
+                { 
+                    //Attack
+                    Console.WriteLine("PlaceHolder attack info " +n+ "PlaceHolder Enemy hits you back");
+                    int damage = p - Program.currentPlayer.armorValue;
+                    if (damage < 0) damage = 0;
+                    int attack = rand.Next(0, Program.currentPlayer.weaponValue) + rand.Next(1,4);
+                    Console.WriteLine("You lose " +damage +" health and you deal " +attack+ " damage");
+                    Program.currentPlayer.health -= damage;
+                    h -= attack;
+                }
+                else if (input.ToLower() == "d" || input.ToLower() == "defend")
+                {
+                    //Defend
+                    Console.WriteLine("PlaceHolder defend info " + n + "PlaceHolder enemy attack into player defensive stance");
+                    int damage = (p/4) - Program.currentPlayer.armorValue;
+                    if (damage < 0) damage = 0;
+                    int attack = rand.Next(0, Program.currentPlayer.weaponValue)/2;
+                    Console.WriteLine("You lose " + damage + " health and you deal " + attack + " damage");
+                    Program.currentPlayer.health -= damage;
+                    h -= attack;
+                }
+                else if (input.ToLower() == "r" || input.ToLower() == "run")
+                {
+                    //Run
+                    if(rand.Next(0, 2) == 0) 
+                    {
+                        Console.WriteLine("PlaceHolder player gets hit trying to run away");
+                        int damage = p - Program.currentPlayer.armorValue;
+                        if (damage < 0)damage = 0;
+                        Console.WriteLine("You Lose "+damage+" health and are unable to escape.");
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        Console.WriteLine("PlaceHolder Escape Text.");
+                        Console.ReadKey();
+                        //go to store
+                    }
+                }
+                if (input.ToLower() == "h" || input.ToLower() == "heal")
+                {
+                    //Heal
+                    if (Program.currentPlayer.potions == 0)
+                    {
+                        Console.WriteLine("PlaceHolder text for no potions");
+                        int damage = p - Program.currentPlayer.armorValue;
+                        if (damage < 0) damage = 0;
+                        Console.WriteLine("You get hit by " + n + " losing " + damage + " health");
+                    }
+                    else
+                    {
+                        int potionV = 5;
+                        Console.WriteLine("You health for " + potionV + " health");
+                        Program.currentPlayer.health += potionV;
+                        int damage = (p/2) - Program.currentPlayer.armorValue;
+                        if(damage < 0)damage = 0;
+                        Console.WriteLine("You lose "+ damage + " health");
+                    }
+                    Console.ReadKey();
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TextBasedRpgProject
+{
+    class Program
+    {
+        public static Player currentPlayer = new Player();
+        static void Main(string[] args)
+        {
+            Start();
+            Encounters.firstEncounter();
+        }
+
+        //Start function dummy
+        static void Start()
+        {
+            Console.WriteLine("PlaceHolder Start Text.");
+            Console.WriteLine("PlaceHolder Text To Ask Player Name.");
+            currentPlayer.name = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("PlaceHolder Story Info");
+            //Dumb Example
+            //Console.WriteLine($"PlaceHolder Text To Greet Player " + currentPlayer.name);
+            if (currentPlayer.name == "")Console.WriteLine("PlaceHolder Memory Loss Text");else Console.WriteLine($"PlaceHolder Greetings Text To Play {currentPlayer.name}");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("PlaceHolder Story Intro");
+            Console.WriteLine("PlaceHolder Descriptive Text");
+
+
+        }
+    }
+}
